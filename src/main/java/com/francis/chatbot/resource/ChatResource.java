01843251e19185c5f7057d5e3330b0b879c043b4
Francis Phiri <franzparks@gmail.com>
2018-01-20 14:03:19 -0800

    Get form data from request

--- src/main/java/com/francis/chatbot/resource/ChatResource.java
+++ src/main/java/com/francis/chatbot/resource/ChatResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.francis.chatbot.model.Message;
@@ -22,8 +23,14 @@ import com.francis.chatbot.model.TextMessage;
 @RequestMapping("/chat")
 public class ChatResource {
 	
-	@RequestMapping(value = "/messages", method = RequestMethod.POST)
-	public Messages chatMessageHandler(){
+	@RequestMapping(value = "/messages", method = RequestMethod.POST,
+			consumes = "multipart/form-data")
+	public Messages chatMessageHandler(
+			@RequestParam(name = "user_id") String userId,
+			@RequestParam(name = "action") String action,
+			@RequestParam(name = "name", required = false) String userName,
+			@RequestParam(name = "text", required = false) String userMessage
+		){
 		Messages messagesList = new Messages();
 		messagesList.addMessage(new TextMessage("Hello User!"));
 		return messagesList;
