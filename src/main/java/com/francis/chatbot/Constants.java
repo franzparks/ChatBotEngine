a699fd25e6472e90c2381deea0a6809607d69e94
Francis Phiri <franzparks@gmail.com>
2018-01-20 13:27:55 -0800

    Add client url

--- src/main/java/com/francis/chatbot/Constants.java
+++ src/main/java/com/francis/chatbot/Constants.java
@@ -8,6 +8,8 @@ package com.francis.chatbot;
  *
  */
 public class Constants {
+	
 	public static final String GMAPS_API_KEY = "AIzaSyD7W7v5psM8TDJwUV2WxsPkoYRtByh07Y0";
 	public static final String DARK_SKY_API_KEY = "8b4d5ca925446f9db4f7d7d0aac8b40c";
+	public static final String 	CLIENT_URL = "http://hipmunk.github.io";
 }
