872ba0ec2f02ca99fd61492899bcd53e60532be7
Francis Phiri <franzparks@gmail.com>
2018-01-20 13:02:37 -0800

    Add RichMessage class

---
+++ src/main/java/com/francis/chatbot/model/RichMessage.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.francis.chatbot.model;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class RichMessage implements Message {
+	
+	private String type;
+	private String html;
+	
+	public RichMessage(String type, String html) {
+	    super();
+	    this.type = "rich";
+	    this.html = html;
+    }
+
+	public String getType() {
+		return type;
+	}
+
+	public String getHtml() {
+		return html;
+	}
+
+	public void setHtml(String html) {
+		this.html = html;
+	}
+	
+	
+	
+	
+}
