7d8e0b20d1657baf13967a4be6463febc0be947e
Francis Phiri <franzparks@gmail.com>
2018-01-20 11:38:20 -0800

    Add application runner class and move app props file to the right place

---
+++ src/main/java/com/francis/chatbot/ChatBotAppRunner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.francis.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author francisphiri
+ *
+ */
+@SpringBootApplication
+public class ChatBotAppRunner {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotAppRunner.class, args);
+	}
+	
+}
