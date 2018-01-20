a7d688a0634f2b24f7b77d9c5c2cde32ebe7ff55
Francis Phiri <franzparks@gmail.com>
2018-01-20 11:54:59 -0800

    Add chat resource controller for handling chat requests

---
+++ src/main/java/com/francis/chatbot/resource/ChatResource.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.francis.chatbot.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author francisphiri
+ *
+ */
+
+@RestController
+@RequestMapping("/chat")
+public class ChatResource {
+	
+}
