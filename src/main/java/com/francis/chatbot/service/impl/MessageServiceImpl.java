de5c0edeb5e8cd561e600ebbb58095506f052997
Francis Phiri <franzparks@gmail.com>
2018-01-20 14:15:35 -0800

    Add MessageService implementor class

---
+++ src/main/java/com/francis/chatbot/service/impl/MessageServiceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.francis.chatbot.service.impl;
+
+import com.francis.chatbot.model.Message;
+import com.francis.chatbot.service.MessageService;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class MessageServiceImpl implements MessageService{
+
+	@Override
+    public Message processMessage(String message) {
+	    
+	    return null;
+    }
+	
+}
