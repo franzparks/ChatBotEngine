28b3b8f9214411822d4fea5990533118cfb8cd83
Francis Phiri <franzparks@gmail.com>
2018-01-20 11:48:10 -0800

    Add constants class and store api keys

---
+++ src/main/java/com/francis/chatbot/Constants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.francis.chatbot;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class Constants {
+	public static final String GMAPS_API_KEY = "AIzaSyD7W7v5psM8TDJwUV2WxsPkoYRtByh07Y0";
+	public static final String DARK_SKY_API_KEY = "8b4d5ca925446f9db4f7d7d0aac8b40c";
+}
