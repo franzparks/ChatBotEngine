4d50ad9d6965121383bfab601ede5b31613e6c96
Francis Phiri <franzparks@gmail.com>
2018-01-20 13:29:05 -0800

    Add config class

---
+++ src/main/java/com/francis/chatbot/config/CorsConfig.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.francis.chatbot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author francisphiri
+ *
+ */
+
+@Configuration
+public class CorsConfig {
+	
+}
