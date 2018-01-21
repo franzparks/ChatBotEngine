b209da6ae6d188a910d8d5a970587d7bf897fbc0
Francis Phiri <franzparks@gmail.com>
2018-01-20 20:12:13 -0800

    Update Weather class

--- src/main/java/com/francis/chatbot/service/model/Weather.java
+++ src/main/java/com/francis/chatbot/service/model/Weather.java
@@ -35,4 +35,32 @@ public class Weather {
 		public String visibility;
 		public String ozone;
     }
+	
+	public Weather(){}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+	
+	
 }
