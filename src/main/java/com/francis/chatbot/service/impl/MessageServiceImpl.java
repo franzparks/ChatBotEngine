e88b88fecc16b034d7225b2ed0fcd44579482b79
Francis Phiri <franzparks@gmail.com>
2018-01-22 21:29:28 -0800

    Add location filter and control message

--- src/main/java/com/francis/chatbot/service/impl/MessageServiceImpl.java
+++ src/main/java/com/francis/chatbot/service/impl/MessageServiceImpl.java
@@ -3,6 +3,9 @@
  */
 package com.francis.chatbot.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -25,6 +28,11 @@ public class MessageServiceImpl implements MessageService{
 	
 	private final RestTemplate restTemplate;
 	private final String errorMessage = "Place cannot be found";
+	private final String controlMessage = "I don't know many things but you can ask me about the weather."
+			+ "\n"
+			+ " I can answer questions such as:" + "\n"
+			+ " what's the weather in <Location>,"+"\n"+ "weather in <Location>,"+"\n"+ "<Location> weather"+"\n"
+			+ " Note: <Location> can be any city or reference to a city such as SF or San Francisco, or 94100";
 	
 	public MessageServiceImpl(RestTemplateBuilder restTemplateBuilder){
 		this.restTemplate = restTemplateBuilder.build();
@@ -35,8 +43,14 @@ public class MessageServiceImpl implements MessageService{
 	 */
 	@Override
     public Message processMessage(String message) {
+		System.out.println(controlMessage);
 	    TextMessage processedMessage = null;
-	    Coordinates coords =  getAddressCoordinates(message);
+	    if(!message.toLowerCase().contains("weather")){
+	    	return new TextMessage(controlMessage);
+	    }
+	    String location = filterLocationFromMessage(message);
+	    
+	    Coordinates coords =  getAddressCoordinates(location);
 	   
 	    if(coords == null){
 	    	processedMessage = new TextMessage(errorMessage);
@@ -53,6 +67,40 @@ public class MessageServiceImpl implements MessageService{
 	    return processedMessage;
     }
 	
+	/**
+	 * Filtering a location from a given message
+	 * @param message
+	 * @return location
+	 */
+	private String filterLocationFromMessage(String message) {
+		
+		//map of helper words which are not a location
+	    Map<String, String> mapOfHelperWords = new HashMap<String, String>();
+	    
+	    String location = "";
+	    
+	    mapOfHelperWords.put("weather", "weather");
+	    mapOfHelperWords.put("in", "in");
+	    mapOfHelperWords.put("the", "the");
+	    mapOfHelperWords.put("like", "like");
+	    mapOfHelperWords.put("forecast", "forecast");
+	    mapOfHelperWords.put("now", "now");
+	    mapOfHelperWords.put("current", "current");
+	    mapOfHelperWords.put("currently", "currently");
+	    mapOfHelperWords.put("what", "what");
+	    mapOfHelperWords.put("what's", "what's");
+	    mapOfHelperWords.put("what is", "what is");
+	    
+	    String[] words = message.split("\\s");
+	    
+	    for(String word : words){
+	    	if(!mapOfHelperWords.containsKey(word.toLowerCase())){
+	    		location += word;
+	    	}
+	    }
+	    return location;
+    }
+
 	/**
 	 * Get coordinates of a given address from the gmap api
 	 * @param address
