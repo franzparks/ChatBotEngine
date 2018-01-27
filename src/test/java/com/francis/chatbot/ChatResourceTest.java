ab873c3937c7475cbba91897da7da569fb1f366f
Francis Phiri <franzparks@gmail.com>
2018-01-27 11:50:24 -0800

    Clean up

--- src/test/java/com/francis/chatbot/ChatResourceTest.java
+++ src/test/java/com/francis/chatbot/ChatResourceTest.java
@@ -3,32 +3,28 @@
  */
 package com.francis.chatbot;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import static org.mockito.BDDMockito.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
 
 import com.francis.chatbot.model.Message;
 import com.francis.chatbot.model.TextMessage;
 import com.francis.chatbot.resource.ChatResource;
 import com.francis.chatbot.service.MessageService;
-
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 /**
  * @author francisphiri
  *
@@ -63,6 +59,7 @@ public class ChatResourceTest {
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.messages", hasSize(1)))
           .andExpect(jsonPath("$.messages[0].text", is("Hello, "+userName + "!")));
+        reset(service);
     }
     
     @Test
@@ -91,6 +88,7 @@ public class ChatResourceTest {
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.messages", hasSize(1)))
           .andExpect(jsonPath("$.messages[0].text", is(controlMessage)));
+        reset(service);
     }
 	
 }
